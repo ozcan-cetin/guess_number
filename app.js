5ddd5161fdb2dc28058b77b432eb37e443947b51
özcan çetin <cetin445@gmail.com>
2022-06-11 01:45:10 +0300

    variables defined

---
+++ app.js
@@ -0,0 +1,10 @@
+/*VARIABLES */
+let start = document.getElementById("start")
+const guess = document.querySelector(".guess");
+const check = document.querySelector(".check");
+const result = document.querySelector(".result")
+const right = document.querySelector(".right")
+const number = document.querySelector(".number")
+const Restart = document.querySelector(".new")
+const errorCheck = document.querySelector(".errorCheck")
+// console.log(guess.value);
